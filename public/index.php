14ebe857516caf3f621b54cc3014723097a12114
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-23 17:53:29 +0100

    Added test routes with regex patterns to index.php

--- public/index.php
+++ public/index.php
@@ -6,19 +6,24 @@ use PHPMin\Router;
 
 $rt = new Router();
 
-$rt->addRoute('GET', '/', function()
-{
-	dump("Home Route");
-});
+//$rt->addRoute('GET', '/', function()
+//{
+	//dump("Home Route");
+//});
 
-$rt->addRoute('GET|POST', '/login', function()
+$rt->addRoute('GET|POST', '/login/', function()
 {
 	dump("Login Route");
 });
 
-$rt->addRoute('GET|POST', '/[a-z]+', function()
+$rt->addRoute('GET|POST', '/[a-z][0-9]$', function()
 {
 	dump("Regex Pattern Route");
+});
+
+$rt->addRoute('GET|POST', '/^[0-9]{3}$', function()
+{
+	dump("Regex Pattern Route With Numbers");
 });
 
 $rt->addRoute('GET', '/dashboard', "DashboardController@index");
