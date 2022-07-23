9357b58834009111de18e924b9a8c93d407fafdc
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-23 09:43:59 +0100

    Added Router::addRoute() method as a way to define routes externally.

--- src/Router.php
+++ src/Router.php
@@ -5,7 +5,25 @@ class Router
 {
 	private $routes = [];
 
-	public function defineRoutes($methods) : array
+
+	/**
+	 * Make a route available to the router.
+	 *
+	 * @param string $methods    A string of request methods seperated
+	 *                           by a "|" character.
+	 *
+	 * @param string $pattern    The url pattern that applies to this route.
+	 *
+	 * @param mixed  $fn         Closure or "nameController@method" can be used
+	 *                           for the defined route,
+	 *
+	 * @return array
+	 */
+	public function addRoute($methods, $pattern, $fn)
+	{
+		$this->loadRoutes($methods, $pattern, $fn);
+	}
+
 	/**
 	 * Load all of the defined routes into the internal routes array.
 	 *
