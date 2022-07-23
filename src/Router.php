851617298997a8b1b281b0ffe66aaead239ea0fd
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-23 09:41:52 +0100

    Added Router::loadRoutes() as a way to define routes for the router to use internally.

--- src/Router.php
+++ src/Router.php
@@ -6,12 +6,27 @@ class Router
 	private $routes = [];
 
 	public function defineRoutes($methods) : array
+	/**
+	 * Load all of the defined routes into the internal routes array.
+	 *
+	 *
+	 * @param string $methods    A string of request methods seperated
+	 *                           by a "|" character.
+	 *
+	 * @param string $pattern    The url pattern that applies to this route.
+	 *
+	 * @param mixed  $fn         Closure or "nameController@method" can be used
+	 *                           for the defined route,
+	 *
+	 * @return array
+	 */
+	private function loadRoutes($methods, $pattern, $fn) : array
 	{
 		$request_methods = explode('|', $methods);
 
 		foreach($request_methods as $method)
 		{
-			$this->routes[] = $method;
+			$this->routes[$method][$pattern] = $fn;
 		}
 
 		return $this->routes;
