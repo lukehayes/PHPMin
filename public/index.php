a412e68ce8d07c078ee3e0234661e55b6e14ccff
Luke Hayes <lukepurplehayes@gmail.com>
2022-07-24 16:05:38 +0100

    Added more test routes to index.php.

--- public/index.php
+++ public/index.php
@@ -6,24 +6,34 @@ use PHPMin\Router;
 
 $rt = new Router();
 
-//$rt->addRoute('GET', '/', function()
-//{
-	//dump("Home Route");
-//});
+$rt->addRoute('GET', '/', function()
+{
+	dump("Home Route");
+});
 
-$rt->addRoute('GET|POST', '/login/', function()
+$rt->addRoute('GET|POST', '/login', function()
 {
 	dump("Login Route");
 });
 
-$rt->addRoute('GET|POST', '/[a-z][0-9]$', function()
+$rt->addRoute('GET|POST', '/posts', function()
+{
+	dump("Post Route");
+});
+
+$rt->addRoute('GET|POST', '/[a-z][0-9]', function()
 {
 	dump("Regex Pattern Route");
 });
 
-$rt->addRoute('GET|POST', '/^[0-9]{3}$', function()
+$rt->addRoute('GET|POST', '/[0-9]{3}', function()
 {
 	dump("Regex Pattern Route With Numbers");
+});
+
+$rt->addRoute('GET|POST', '/[0-9]{2}[a-z]{2}', function()
+{
+	dump("Regex Pattern Route With Numbers EXTRA");
 });
 
 $rt->addRoute('GET', '/dashboard', "DashboardController@index");
