ec1a7005b6ab05893d7b39f4a31bb96a8ebb711c
lukehayes <lukepurplehayes@gmail.com>
2022-11-29 17:23:20 +0000

    Moved test DashboardController into src/controllers.

--- public/index.php
+++ public/index.php
@@ -8,29 +8,27 @@ $router = new Router();
 
 $router->addRoute('GET', '/', function()
 {
-	dump("Home Route");
+    dump("Home Route");
 });
 
 $router->addRoute('GET|POST', '/login', function()
 {
-	dump("Login Route");
+    dump("Login Route");
 });
 
 $router->addRoute('GET|POST', '/posts', function()
 {
-	dump("Post Route");
+    dump("Post Route");
 });
 
 $router->addRoute('GET|POST', '/[a-z][0-9]', function()
 {
-	dump("Regex Pattern Route");
+    dump("Regex Pattern Route");
 });
 
 $router->addRoute('GET|POST', '/[0-9]{3}', function()
 {
-	dump("Regex Pattern Route With Numbers");
+    dump("Regex Pattern Route With Numbers");
 });
-
-$router->addRoute('GET', '/dashboard', "DashboardController@index");
 
 $router->run();
